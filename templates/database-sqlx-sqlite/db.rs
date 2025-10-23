c7f328dd3960099b4bdeb96db50d5c7e4bc6bdbe
Nkemjika <57384968+Nkemjikanma@users.noreply.github.com>
2025-10-23 21:19:29 +0100

    feat: add docker compose to postgres (#7)
    * feat: add docker file and dynamic port
    
    * fix: fix dynamic port selection if default is busy
    
    * chore: update readme
    
    * ci: comment out workflow for integration test
    
    * chore: update readme for postgres projects
    
    * chore: bump version to 0.4.0

--- templates/database-sqlx-sqlite/db.rs
+++ templates/database-sqlx-sqlite/db.rs
@@ -1,6 +1,9 @@
 use super::config::DBConfig;
 use sqlx::sqlite::{SqliteConnectOptions, SqlitePool, SqlitePoolOptions};
+<<<<<<< HEAD
 
+=======
+>>>>>>> 56bc7a2 (feat: add docker compose to postgres (#7))
 use std::str::FromStr;
 
 #[tracing::instrument(name = "pool", skip_all)]
